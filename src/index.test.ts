9c39b63d248fbded97ccab8429ee61486d8581ce
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-11-28 21:14:15 +1100

    test: high nesting

--- src/index.test.ts
+++ src/index.test.ts
@@ -93,4 +93,28 @@ describe('Advance merge', () => {
       expectedQueryString: "SELECT Id FROM Object WHERE (field1 = 'value1' AND field3 = 'value3') OR (field2 = 'value2' OR field4 = 'value4')",
     });
   });
+
+  test('extremely high nesting in WHERE clauses with AND', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        ((field1 = 'value1' AND (field2 = 'value2' OR (field3 = 'value3' AND field4 = 'value4'))) 
+         OR ((field5 = 'value5' AND field6 = 'value6') AND (field7 = 'value7' OR field8 = 'value8')))
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE
+        (((field9 = 'value9' AND (field10 = 'value10' OR field11 = 'value11')) OR field12 = 'value12') 
+         AND ((field13 = 'value13' OR field14 = 'value14') AND (field15 = 'value15' AND field16 = 'value16')))
+      `,
+      operator: 'AND',
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (((field1 = 'value1' AND (field2 = 'value2' OR (field3 = 'value3' AND field4 = 'value4'))) 
+          OR ((field5 = 'value5' AND field6 = 'value6') AND (field7 = 'value7' OR field8 = 'value8')))) 
+        AND 
+        ((((field9 = 'value9' AND (field10 = 'value10' OR field11 = 'value11')) OR field12 = 'value12') 
+          AND ((field13 = 'value13' OR field14 = 'value14') AND (field15 = 'value15' AND field16 = 'value16'))))
+      `,
+    });
+  });
 });
