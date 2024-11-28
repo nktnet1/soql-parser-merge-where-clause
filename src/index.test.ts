c2d6b8f5483b157f5f7d7c7a5ecfcd7a7d99643f
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-11-28 21:19:46 +1100

    test: subquery test case

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,11 +3,12 @@ import { mergeWhereClauses } from './index';
 import { composeQuery, formatQuery, LogicalOperator, parseQuery } from 'soql-parser-js';
 import { format } from 'sql-formatter';
 
-function testMergeWhereClauses({ queryString1, queryString2, operator, expectedQueryString }: {
+function testMergeWhereClauses({ queryString1, queryString2, operator, expectedQueryString, useStrict = true }: {
   queryString1: string;
   queryString2: string;
   operator: LogicalOperator;
   expectedQueryString: string;
+  useStrict?: boolean;
 }) {
   const query1 = parseQuery(queryString1);
   const query2 = parseQuery(queryString2);
@@ -22,8 +23,12 @@ function testMergeWhereClauses({ queryString1, queryString2, operator, expectedQ
   expect(() => formatQuery(queryAfterMerge)).not.toThrow();
   expect(() => format(queryAfterMerge)).not.toThrow();
 
-  // Equality match for the generated structure
-  expect(mergedWhere).toStrictEqual(expectedResult!);
+
+  if (useStrict) {
+    expect(mergedWhere).toStrictEqual(expectedResult!);
+  } else {
+    expect(mergedWhere).toEqual(expectedResult!);
+  }
 }
 
 describe('Base case merge', () => {
@@ -75,7 +80,7 @@ describe('Basic merge', () => {
   });
 });
 
-describe('Advance merge', () => {
+describe('Nested AND/OR', () => {
   test('nested WHERE clauses with AND', () => {
     testMergeWhereClauses({
       queryString1: "SELECT Id FROM Object WHERE field1 = 'value1' AND field3 = 'value3'",
@@ -115,6 +120,62 @@ describe('Advance merge', () => {
         ((((field9 = 'value9' AND (field10 = 'value10' OR field11 = 'value11')) OR field12 = 'value12') 
           AND ((field13 = 'value13' OR field14 = 'value14') AND (field15 = 'value15' AND field16 = 'value16'))))
       `,
+    });
+  });
+});
+
+describe('Subqueries', () => {
+  /*
+   * Note: avoid `.toStrictEqual` since in Jest/Vitest, SoqlQuery gets converted to "Object"
+   * Further reading: https://backend.cafe/should-you-use-jest-as-a-testing-library
+   */
+  test('WHERE clauses with subqueries using AND', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
+        AND field3 = 'value3'
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE 
+        field4 = 'value4' 
+        AND field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5')
+      `,
+      operator: 'AND',
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
+         AND field3 = 'value3') 
+        AND 
+        (field4 = 'value4' 
+         AND field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5'))
+      `,
+      useStrict: false,
+    });
+  });
+
+  test('WHERE clauses with subqueries using OR', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
+        OR field3 = 'value3'
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE 
+        field4 = 'value4' 
+        OR field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5')
+      `,
+      operator: 'OR',
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
+         OR field3 = 'value3') 
+        OR 
+        (field4 = 'value4' 
+         OR field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5'))
+      `,
+      useStrict: false,
     });
   });
 });
