fdbb2bd09c3f1774061568ab57930cc331a273af
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-11-28 21:43:01 +1100

    test: finalised test suite

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,13 @@ import { mergeWhereClauses } from './index';
 import { composeQuery, formatQuery, LogicalOperator, parseQuery } from 'soql-parser-js';
 import { format } from 'sql-formatter';
 
-function testMergeWhereClauses({ queryString1, queryString2, operator, expectedQueryString, useStrict = true }: {
+function testMergeWhereClauses({
+  queryString1,
+  queryString2,
+  operator,
+  expectedQueryString,
+  useStrict = true,
+}: {
   queryString1: string;
   queryString2: string;
   operator: LogicalOperator;
@@ -23,79 +29,99 @@ function testMergeWhereClauses({ queryString1, queryString2, operator, expectedQ
   expect(() => formatQuery(queryAfterMerge)).not.toThrow();
   expect(() => format(queryAfterMerge)).not.toThrow();
 
-
   if (useStrict) {
     expect(mergedWhere).toStrictEqual(expectedResult!);
   } else {
+    /*
+     * NOTE:
+     * - avoid `.toStrictEqual` since in Jest/Vitest, SoqlQuery gets converted to "Object"
+     * - This is relevant only for the subqueries test cases
+     * - Further reading: https://backend.cafe/should-you-use-jest-as-a-testing-library
+     */
     expect(mergedWhere).toEqual(expectedResult!);
   }
 }
 
 describe('Base case merge', () => {
-  test('No WHERE clause in either query', () => {
-    testMergeWhereClauses({
+  test.each([
+    {
       queryString1: "SELECT Id FROM Object",
       queryString2: "SELECT Id FROM Object",
-      operator: 'AND',
-      expectedQueryString: "SELECT Id FROM Object",
-    });
-  });
-
-  test('empty WHERE clauses in first query', () => {
-    testMergeWhereClauses({
+      operator: 'AND' as LogicalOperator,
+      expectedQueryString: "SELECT Id FROM Object"
+    },
+    {
       queryString1: "SELECT Id FROM Object",
       queryString2: "SELECT Id FROM Object WHERE field1 = 'value1'",
-      operator: 'AND',
-      expectedQueryString: "SELECT Id FROM Object WHERE field1 = 'value1'",
-    });
-  });
-
-  test('empty WHERE clauses in second query', () => {
+      operator: 'AND' as LogicalOperator,
+      expectedQueryString: "SELECT Id FROM Object WHERE field1 = 'value1'"
+    },
+  ])('merge basic WHERE clauses - $expectedQueryString', ({ queryString1, queryString2, operator, expectedQueryString }) => {
     testMergeWhereClauses({
-      queryString1: "SELECT Id FROM Object WHERE field1 = 'value1'",
-      queryString2: "SELECT Id FROM Object",
-      operator: 'AND',
-      expectedQueryString: "SELECT Id FROM Object WHERE field1 = 'value1'",
+      queryString1,
+      queryString2,
+      operator,
+      expectedQueryString,
     });
   });
 });
 
-describe('Basic merge', () => {
-  test('two simple WHERE clauses with AND', () => {
-    testMergeWhereClauses({
+describe('Basic merge with AND/OR conditions', () => {
+  test.each([
+    {
+      operator: 'AND' as LogicalOperator,
       queryString1: "SELECT Id FROM Object WHERE field1 = 'value1'",
       queryString2: "SELECT Id FROM Object WHERE field2 = 'value2'",
-      operator: 'AND',
-      expectedQueryString: "SELECT Id FROM Object WHERE (field1 = 'value1') AND (field2 = 'value2')",
-    });
-  });
-
-  test('two simple WHERE clauses with OR', () => {
-    testMergeWhereClauses({
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1') AND (field2 = 'value2')
+      `
+    },
+    {
+      operator: 'OR' as LogicalOperator,
       queryString1: "SELECT Id FROM Object WHERE field1 = 'value1'",
       queryString2: "SELECT Id FROM Object WHERE field2 = 'value2'",
-      operator: 'OR',
-      expectedQueryString: "SELECT Id FROM Object WHERE (field1 = 'value1') OR (field2 = 'value2')",
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1') OR (field2 = 'value2')
+      `
+    },
+  ])('should merge WHERE clauses with $operator', ({ operator, queryString1, queryString2, expectedQueryString }) => {
+    testMergeWhereClauses({
+      queryString1,
+      queryString2,
+      operator,
+      expectedQueryString,
     });
   });
 });
 
 describe('Nested AND/OR', () => {
-  test('nested WHERE clauses with AND', () => {
-    testMergeWhereClauses({
+  test.each([
+    {
+      operator: 'AND' as LogicalOperator,
       queryString1: "SELECT Id FROM Object WHERE field1 = 'value1' AND field3 = 'value3'",
       queryString2: "SELECT Id FROM Object WHERE field2 = 'value2' OR field4 = 'value4'",
-      operator: 'AND',
-      expectedQueryString: "SELECT Id FROM Object WHERE (field1 = 'value1' AND field3 = 'value3') AND (field2 = 'value2' OR field4 = 'value4')",
-    });
-  });
-
-  test('nested WHERE clauses with OR', () => {
-    testMergeWhereClauses({
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1' AND field3 = 'value3') AND (field2 = 'value2' OR field4 = 'value4')
+      `
+    },
+    {
+      operator: 'OR' as LogicalOperator,
       queryString1: "SELECT Id FROM Object WHERE field1 = 'value1' AND field3 = 'value3'",
       queryString2: "SELECT Id FROM Object WHERE field2 = 'value2' OR field4 = 'value4'",
-      operator: 'OR',
-      expectedQueryString: "SELECT Id FROM Object WHERE (field1 = 'value1' AND field3 = 'value3') OR (field2 = 'value2' OR field4 = 'value4')",
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1' AND field3 = 'value3') OR (field2 = 'value2' OR field4 = 'value4')
+      `
+    },
+  ])('should merge nested WHERE clauses with $operator', ({ operator, queryString1, queryString2, expectedQueryString }) => {
+    testMergeWhereClauses({
+      queryString1,
+      queryString2,
+      operator,
+      expectedQueryString,
     });
   });
 
@@ -111,7 +137,7 @@ describe('Nested AND/OR', () => {
         (((field9 = 'value9' AND (field10 = 'value10' OR field11 = 'value11')) OR field12 = 'value12') 
          AND ((field13 = 'value13' OR field14 = 'value14') AND (field15 = 'value15' AND field16 = 'value16')))
       `,
-      operator: 'AND',
+      operator: 'AND' as LogicalOperator,
       expectedQueryString: `
         SELECT Id FROM Object WHERE 
         (((field1 = 'value1' AND (field2 = 'value2' OR (field3 = 'value3' AND field4 = 'value4'))) 
@@ -125,12 +151,9 @@ describe('Nested AND/OR', () => {
 });
 
 describe('Subqueries', () => {
-  /*
-   * Note: avoid `.toStrictEqual` since in Jest/Vitest, SoqlQuery gets converted to "Object"
-   * Further reading: https://backend.cafe/should-you-use-jest-as-a-testing-library
-   */
-  test('WHERE clauses with subqueries using AND', () => {
-    testMergeWhereClauses({
+  test.each([
+    {
+      operator: 'AND' as LogicalOperator,
       queryString1: `
         SELECT Id FROM Object WHERE 
         field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
@@ -141,7 +164,6 @@ describe('Subqueries', () => {
         field4 = 'value4' 
         AND field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5')
       `,
-      operator: 'AND',
       expectedQueryString: `
         SELECT Id FROM Object WHERE 
         (field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
@@ -149,13 +171,10 @@ describe('Subqueries', () => {
         AND 
         (field4 = 'value4' 
          AND field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5'))
-      `,
-      useStrict: false,
-    });
-  });
-
-  test('WHERE clauses with subqueries using OR', () => {
-    testMergeWhereClauses({
+      `
+    },
+    {
+      operator: 'OR' as LogicalOperator,
       queryString1: `
         SELECT Id FROM Object WHERE 
         field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
@@ -166,7 +185,6 @@ describe('Subqueries', () => {
         field4 = 'value4' 
         OR field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5')
       `,
-      operator: 'OR',
       expectedQueryString: `
         SELECT Id FROM Object WHERE 
         (field1 IN (SELECT Id FROM RelatedObject WHERE field2 = 'value1') 
@@ -174,8 +192,77 @@ describe('Subqueries', () => {
         OR 
         (field4 = 'value4' 
          OR field5 IN (SELECT Id FROM AnotherRelatedObject WHERE field6 = 'value5'))
-      `,
+      `
+    },
+  ])('should merge WHERE clauses with subqueries using $operator', ({ operator, queryString1, queryString2, expectedQueryString }) => {
+    testMergeWhereClauses({
+      queryString1,
+      queryString2,
+      operator,
+      expectedQueryString,
       useStrict: false,
+    });
+  });
+});
+
+describe('Contains NOT', () => {
+  test('should merge WHERE clauses with mixed AND, OR, NOT operators', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        field1 = 'value1' AND field2 = 'value2' OR NOT field3 = 'value3'
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE 
+        field4 = 'value4' AND NOT field5 = 'value5'
+      `,
+      operator: 'AND' as LogicalOperator,
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1' AND field2 = 'value2' OR NOT field3 = 'value3') 
+        AND 
+        (field4 = 'value4' AND NOT field5 = 'value5')
+      `
+    });
+  });
+
+  test('should handle AND and NOT operators correctly when combined', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        field1 = 'value1' AND NOT field2 = 'value2'
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE 
+        field3 = 'value3' AND NOT field4 = 'value4'
+      `,
+      operator: 'AND' as LogicalOperator,
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1' AND NOT field2 = 'value2') 
+        AND 
+        (field3 = 'value3' AND NOT field4 = 'value4')
+      `
+    });
+  });
+
+  test('should handle OR and NOT operators correctly when combined', () => {
+    testMergeWhereClauses({
+      queryString1: `
+        SELECT Id FROM Object WHERE 
+        field1 = 'value1' OR NOT field2 = 'value2'
+      `,
+      queryString2: `
+        SELECT Id FROM Object WHERE 
+        field3 = 'value3' OR NOT field4 = 'value4'
+      `,
+      operator: 'OR' as LogicalOperator,
+      expectedQueryString: `
+        SELECT Id FROM Object WHERE 
+        (field1 = 'value1' OR NOT field2 = 'value2') 
+        OR 
+        (field3 = 'value3' OR NOT field4 = 'value4')
+      `
     });
   });
 });
