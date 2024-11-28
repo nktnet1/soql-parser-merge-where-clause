703a725928e157e6e16c08d61f2f90829ae79b70
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-11-28 22:34:11 +1100

    docs: jsdoc added above functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import type { WhereClause, LogicalOperator } from "soql-parser-js";
 
+/**
+ * Wraps the given `WhereClause` in parentheses - useful for merging.
+ *
+ * @param {WhereClause} clause - The `WhereClause` to be wrapped.
+ * @returns {{beginClause: WhereClause, endClause: WhereClause}} An object
+ *          containing the `beginClause` (the wrapped version of the input
+ *          `clause`) and the `endClause` (the last clause where the right
+ *          side of the expression ends with a close parenthesis).
+ */
 const wrapWhereClauseInParenthesis = (clause: WhereClause): { beginClause: WhereClause, endClause: WhereClause } => {
   const clone = JSON.parse(JSON.stringify(clause)) as WhereClause;
   clone.left.openParen = (clone.left.openParen ?? 0) + 1;
@@ -11,6 +20,18 @@ const wrapWhereClauseInParenthesis = (clause: WhereClause): { beginClause: Where
   return { beginClause: clone, endClause: current };
 };
 
+/**
+ * Merges two `WhereClause` objects into a single clause, joining them with a
+ * logical operator.
+ *
+ * @param {WhereClause} [where1] - First clause to merge. Optional.
+ * @param {WhereClause} [where2] - Second clause to merge. Optional.
+ * @param {LogicalOperator} [operator='AND'] - Operator used to join the clauses 
+ * (default is 'AND'). Optional.
+ *
+ * @returns {WhereClause|undefined} A new `WhereClause` that represents the merged
+ * expression, or `undefined` if both clauses are not provided.
+ */
 export const mergeWhereClauses = (
   where1?: WhereClause,
   where2?: WhereClause,
